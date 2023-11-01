e3b8c4b299a620f8e2f9b32d1422a638ced3c696
Ramil-Karipov <omil8@yandex.ru>
2023-11-02 00:08:41 +0500

    task11

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        public static void task1 ( int year = 2024){
-            if ((year % 4 == 0) && year % 100 != 0) {
-                System.out.println(year + " год является високосным");
-            } else if (year % 400 == 0) {
-                System.out.println(year + " год является високосным");
-            } else {
-                System.out.println(year + " год является не високосным");
-            }
+        task1();
+    }
+
+    public static void task1() {
+        if ((year % 4 == 0) && year % 100 != 0) {
+            System.out.println(year + " год является високосным");
+        } else if (year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год является не високосным");
+
+
         }
     }
 }
