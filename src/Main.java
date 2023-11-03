d8ae2657389c79a258f649c616d091c2d0b0f7fd
Ramil-Karipov <omil8@yandex.ru>
2023-11-03 13:33:31 +0500

    finish1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 class homework {
     public static void main(String[] args) {
         task1(2024);
-        task2(1, 2016);
-        task3(98);
+        task2(0, 2013);
+        task3(13);
 
     }
 
@@ -15,35 +15,43 @@ class homework {
         }
     }
 
-    public static void task2(int clientOs, int clientDeviceYear) {
+    public static void task2(int client, int clientDeviceYear) {
         int ios = 1;
         int android = 0;
-        if (clientOs == ios && (clientDeviceYear <= 2015 || clientDeviceYear > 2015)) {
+        boolean condition = clientDeviceYear <= 2015 || clientDeviceYear > 2015;
+        if (client == ios && (condition)) {
             System.out.println("Установите версию приложения для iOS по ссылке");
             if (clientDeviceYear <= 2015) {
                 System.out.println("Установите облегчённую версию приложения для iOS по ссылке");
             }
         }
+        if (client == android && (condition)) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+            if (clientDeviceYear <= 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        }
     }
 
     public static void task3(int deliveryDistance) {
-        boolean oneDay = (deliveryDistance < 20);
-        if (oneDay) {
+        if (deliveryDistance < 20) {
             System.out.println("Потребуется 1 день");
-        } else {
-            boolean twoDays = (deliveryDistance > 20) && (deliveryDistance < 60);
-            if (twoDays) {
-                System.out.println("Потребуется 2 дня");
-            } else {
-                boolean threedays = (deliveryDistance > 60) && (deliveryDistance < 100);
-                if (threedays) {
-                    System.out.println("Потребуется 3 дня");
-                } else {
-                    System.out.println("Доставка не предусмотрена");
-                }
-
-            }
-
+        } else if (deliveryDistance > 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется 2 дня");
+        }
+        if (deliveryDistance > 60 && deliveryDistance < 100) {
+            System.out.println("Потребуется 3 дня");
+        } else if (deliveryDistance > 100) {
+            System.out.println("Доставка не предусмотрена");
         }
     }
+
 }
+
+
+
+
+
+
+
+
